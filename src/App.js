0e532290c63c4da418b60d30684e5c81e3eb2cd9
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-10 15:24:36 +0600

    added navbar, declare and created all routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/Appointment/Appointment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Appointment = () => {
+  return <>APPOINTMENT</>;
+};
+
+export default Appointment;
