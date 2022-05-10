1c1954de92ad8afbca02f51da1a2f7e6c4ff7ea2
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-10 22:55:24 +0600

    added contatc us section

---
+++ src/Pages/Home/Contact.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import appointment from '../../assets/images/appointment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const Contact = () => {
+  return (
+    <>
+      <section
+        className="text-white flex justify-center items-center mb-12 mt-40"
+        style={{
+          background: `url(${appointment})`,
+          backgroundSize: 'cover',
+        }}
+      >
+        <section className=" body-font relative">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-12">
+              <h3 className="text-xl text-primary font-bold">Contact Us</h3>
+              <h2 className="text-3xl">Stay connected with us</h2>
+            </div>
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <div className="flex flex-wrap -m-2">
+                {/* Email */}
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label for="email" className="leading-7 text-sm ">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                {/* Subject */}
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label for="name" className="leading-7 text-sm ">
+                      Subject
+                    </label>
+                    <input
+                      type="text"
+                      id="Subject"
+                      name="Subject"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                {/* Message */}
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label for="message" className="leading-7 text-sm ">
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="p-2 mx-auto">
+                  <PrimaryButton>Submit</PrimaryButton>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
