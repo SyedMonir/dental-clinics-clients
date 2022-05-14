b7105ec1d89ee01631e643d45661ad3830ab2d61
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-14 20:14:57 +0600

    added my appointment section and fixed some issue

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const MyAppointment = () => {
+  const [user, userLoading, userError] = useAuthState(auth);
+  if (userError) {
+    // console.log(error);
+  }
+  const {
+    isLoading,
+    error,
+    data: appointments,
+  } = useQuery(['bookings'], () =>
+    fetch(`http://localhost:5000/booking?patient=${user?.email}`).then((res) =>
+      res.json()
+    )
+  );
+  console.log(appointments);
+
+  if (isLoading || userLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    console.log('An error has occurred: ' + error.message);
+  }
   return (
     <>
-      <h3>My Appointment</h3>
+      <div className="overflow-x-auto mx-2">
+        <table className="table table-zebra w-full table-normal">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments?.map((a, index) => (
+              <tr key={a._id} className="hover">
+                <th>{index + 1}</th>
+                <td>{a?.patientName}</td>
+                <td>{a?.date}</td>
+                <td>{a?.slot}</td>
+                <td>{a?.treatment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <nav className="navbar sticky top-0 z-50 bg-base-100 justify-between shadow-lg uppercase px-12">
+      <nav className="navbar sticky top-0 z-50 bg-base-100 justify-between shadow-lg uppercase px-12 mb-4">
         {/* For Mobile */}
         <div className="navbar-start">
           <div className="dropdown">
@@ -72,7 +72,7 @@ const Navbar = () => {
               </svg>
             </label>
             <ul
-              tabIndex="0"
+              tabIndex="1"
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               {menuItem}
@@ -86,6 +86,30 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">{menuItem}</ul>
         </div>
+        {user && (
+          <div className="navbar-end">
+            <label
+              tabIndex="2"
+              htmlFor="dashboard-sidebar"
+              className="btn btn-ghost lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+          </div>
+        )}
       </nav>
     </>
   );
